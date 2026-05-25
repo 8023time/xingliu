8b80775f392ccdd1cc94f92992628e6cc2be6af3
8023time <2162105974@qq.com>
2026-05-25 17:45:26 +0800

    feat: 添加scalar辅助swagger显示api文档

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,12 +3,15 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from '@libs/common/filter/http-exception.filter';
 import { HttpResponseInterceptor } from '@libs/common/interceptor/http-response.interceptor';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new HttpResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
     .setTitle('AI Creator Platform API')
@@ -19,6 +22,13 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
+
+  app.use(
+    '/docs',
+    apiReference({
+      content: document,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
