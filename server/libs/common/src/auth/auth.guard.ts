c953f827d6046fb59641fb45a0d1ca5d5f7fb9de
8023time <2162105974@qq.com>
2026-06-03 21:01:21 +0800

    fix: 修改鉴权方式,添加查询数据库+token版本号两层验证

--- server/libs/common/src/auth/auth.guard.ts
+++ server/libs/common/src/auth/auth.guard.ts
@@ -1,17 +1,21 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import type { AuthTokenPayloadType } from '@xingliu/shared/user';
 import { JwtService } from '@nestjs/jwt';
-import { Observable } from 'rxjs';
 import type { Request } from 'express';
+import { PrismaService } from '@libs/common';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest<Request & { user?: AuthTokenPayloadType }>();
     const Headers = req.headers; // 获取请求头信息
     const authorization = Headers.authorization; // 获取请求头信息
+
     if (!authorization) {
       throw new UnauthorizedException('未授权!!!');
     }
@@ -19,10 +23,26 @@ export class AuthGuard implements CanActivate {
 
     try {
       const payload = this.jwtService.verify<AuthTokenPayloadType>(token);
+
+      // 验证 token 的类型，确保它是一个访问令牌
       if (payload.tokenType !== 'access') {
         throw new UnauthorizedException('token 过期或无效!!!');
       }
-      req.user = payload; // 将用户信息保存在请求中
+
+      // 验证用户是否存在
+      const user = await this.prismaService.user.findUnique({ where: { id: payload.userId } });
+
+      if (!user) {
+        throw new UnauthorizedException('用户不存在!!!');
+      }
+
+      // 验证 tokenVersion 是否匹配，确保令牌未过期
+      if (payload.tokenVersion !== user.tokenVersion) {
+        throw new UnauthorizedException('token 过期或无效!!!');
+      }
+
+      // 将用户信息保存在请求中
+      req.user = payload;
       return true;
     } catch {
       throw new UnauthorizedException('token 过期或无效!!!');
